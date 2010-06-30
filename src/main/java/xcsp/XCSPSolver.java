f758948c19afe3def493d4f501092b2956ad4321
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2010-06-30 16:02:49 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/examples@522 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/java/xcsp/XCSPSolver.java
+++ src/main/java/xcsp/XCSPSolver.java
@@ -6,10 +6,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import concrete.functionaltests.ConcreteTest;
 import cspfj.MGACIter;
 import cspfj.exception.FailedGenerationException;
+import cspfj.filter.AC3Constraint;
 import cspfj.generator.ProblemGenerator;
+import cspfj.priorityqueues.Fifo;
 import cspfj.problem.Problem;
 import cspom.CSPParseException;
 import cspom.compiler.ProblemCompiler;
@@ -18,7 +19,7 @@ import cspom.constraint.CSPOMConstraint;
 public class XCSPSolver {
 	private static int count(final String name) throws CSPParseException,
 			IOException, FailedGenerationException {
-		final cspom.CSPOM cspomProblem = cspom.CSPOM.load(ConcreteTest.class
+		final cspom.CSPOM cspomProblem = cspom.CSPOM.load(XCSPSolver.class
 				.getResource(name));
 		ProblemCompiler.compile(cspomProblem);
 		final Problem problem = ProblemGenerator.generate(cspomProblem);
@@ -50,7 +51,40 @@ public class XCSPSolver {
 		return count;
 	}
 
-	public static void main(String[] args) throws CSPParseException, IOException, FailedGenerationException {
-		System.out.println(count("frb35-17-1_ext.xml.bz2"));
+	private static boolean solve(final String name) throws CSPParseException,
+			IOException, FailedGenerationException {
+		final cspom.CSPOM cspomProblem = cspom.CSPOM.load(XCSPSolver.class
+				.getResource(name));
+		ProblemCompiler.compile(cspomProblem);
+		final Problem problem = ProblemGenerator.generate(cspomProblem);
+		// System.out.println(problem);
+
+		final MGACIter solver = new MGACIter(problem,
+				new AC3Constraint(problem));
+
+		final Map<String, Integer> solution = solver.nextSolution();
+
+		if (solution == null) {
+			return false;
+		}
+		final Map<String, Number> numSolution = new LinkedHashMap<String, Number>(
+				solution.size());
+		for (Entry<String, Integer> e : solution.entrySet()) {
+			numSolution.put(e.getKey(), e.getValue());
+		}
+		final Collection<CSPOMConstraint> failed = cspomProblem
+				.control(numSolution);
+		if (!failed.isEmpty()) {
+			throw new IllegalStateException(failed.toString());
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) throws CSPParseException,
+			IOException, FailedGenerationException {
+		System.out.println(solve("frb35-17-1_ext.xml.bz2"));
+		System.out.println(Fifo.insert + " inserts, " + Fifo.update
+				+ " updates, " + Fifo.remove + " removes");
 	}
 }
