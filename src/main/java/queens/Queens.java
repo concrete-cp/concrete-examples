d702627bcd7bcf7869814e33bd13c05c54885859
scand1sk <scand1sk@b3bf8218-cbbe-40ea-ae7b-cf512e877a40>
2010-09-24 10:13:41 +0000

    git-svn-id: https://svn.code.sf.net/p/cspfj/code/trunk/examples@551 b3bf8218-cbbe-40ea-ae7b-cf512e877a40

--- src/main/java/queens/Queens.java
+++ src/main/java/queens/Queens.java
@@ -10,6 +10,7 @@ import cspfj.AbstractSolver;
 import cspfj.MGACIter;
 import cspfj.ParameterManager;
 import cspfj.Solver;
+import cspfj.StatisticsManager;
 import cspfj.exception.FailedGenerationException;
 import cspfj.generator.ProblemGenerator;
 import cspom.CSPOM;
@@ -19,57 +20,58 @@ import cspom.compiler.ProblemCompiler;
 import cspom.variable.CSPOMVariable;
 
 public final class Queens {
-	private final int size;
-	private final CSPOMVariable[] variables;
+    private final int size;
+    private final CSPOMVariable[] variables;
 
-	private Queens(final int size) {
-		this.size = size;
-		variables = new CSPOMVariable[size];
-	}
+    private Queens(final int size) {
+        this.size = size;
+        variables = new CSPOMVariable[size];
+    }
 
-	public CSPOM generate() throws PredicateParseException,
-			DuplicateVariableException {
-		final CSPOM problem = new CSPOM();
+    public CSPOM generate() throws PredicateParseException,
+            DuplicateVariableException {
+        final CSPOM problem = new CSPOM();
 
-		for (int i = size; --i >= 0;) {
-			variables[i] = problem.var("Q" + i, 1, size);
-		}
+        for (int i = size; --i >= 0;) {
+            variables[i] = problem.var("Q" + i, 1, size);
+        }
 
-		for (int j = size; --j >= 0;) {
-			for (int i = j; --i >= 0;) {
-				problem.ctr("ne(" + variables[i] + ", " + variables[j] + ")");
-				problem.ctr("ne(abs(sub(" + variables[i] + ", " + variables[j]
-						+ ")), " + (j - i) + ")");
-			}
-		}
+        for (int j = size; --j >= 0;) {
+            for (int i = j; --i >= 0;) {
+                problem.ctr("ne(" + variables[i] + ", " + variables[j] + ")");
+                problem.ctr("ne(abs(sub(" + variables[i] + ", " + variables[j]
+                        + ")), " + (j - i) + ")");
+            }
+        }
 
-		return problem;
-	}
+        return problem;
+    }
 
-	public static void main(String[] args) throws FailedGenerationException,
-			NumberFormatException, PredicateParseException,
-			DuplicateVariableException, IOException, ClassNotFoundException {
-		ParameterManager.parameterParse("logger.level", "INFO");
-		ParameterManager.parameterParse("heuristic.variable", "cspfj.heuristic.WDegFixedOnDom");
-		for (int i : Arrays.asList(4, 8, 12, 15, 20, 30, 50, 80, 100, 120, 150)) {
-			System.out.println(i + " :");
-			long time = -System.currentTimeMillis();
-			final Queens queens = new Queens(i);
-			final CSPOM problem = queens.generate();
-			ProblemCompiler.compile(problem);
+    public static void main(String[] args) throws FailedGenerationException,
+            NumberFormatException, PredicateParseException,
+            DuplicateVariableException, IOException, ClassNotFoundException {
+        ParameterManager.parameterParse("logger.level", "INFO");
+        ParameterManager.parameterParse("heuristic.variable",
+                "cspfj.heuristic.WDegFixedOnDom");
+        for (int i : Arrays.asList(4, 8, 12, 15, 20, 30, 50, 80, 100, 120, 150)) {
+            System.out.println(i + " :");
+            long time = -System.currentTimeMillis();
+            final Queens queens = new Queens(i);
+            final CSPOM problem = queens.generate();
+            ProblemCompiler.compile(problem);
 
-			final Solver solver = new MGACIter(
-					ProblemGenerator.generate(problem));
+            final Solver solver = AbstractSolver.factory(problem);
 
-			Map<String, Integer> solution = solver.nextSolution();
-			int count = 0;
-			System.out.println((System.currentTimeMillis() + time) / 1000f);
-			// while (solution != null) {
-			// solution = solver.nextSolution();
-			// count++;
-			// }
-			// System.out.println(count + " in "
-			// + (System.currentTimeMillis() + time) / 1e3f);
-		}
-	}
+            Map<String, Integer> solution = solver.nextSolution();
+            int count = 0;
+            System.out.println((System.currentTimeMillis() + time) / 1000f);
+            System.out.println(StatisticsManager.digest());
+            // while (solution != null) {
+            // solution = solver.nextSolution();
+            // count++;
+            // }
+            // System.out.println(count + " in "
+            // + (System.currentTimeMillis() + time) / 1e3f);
+        }
+    }
 }
