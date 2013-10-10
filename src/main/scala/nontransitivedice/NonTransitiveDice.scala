a8928be707548059c7e2a8518209feb02458c60b
Julien Vion <julien.vion@univ-valenciennes.fr>
2013-10-10 12:11:50 +0200

    Added the Non-Transitive-Dice example Simplified the Groups problem to take advantage of Concrete's optimization features and new CSPOM modelling facilities

---
+++ src/main/scala/nontransitivedice/NonTransitiveDice.scala
@@ -0,0 +1,63 @@
+package nontransitivedice
+
+import cspom.CSPOM
+import CSPOM._
+import cspom.variable.IntVariable
+import concrete.CSPOMDriver._
+import concrete.Solver
+
+object NonTransitiveDice extends App {
+  val NB_DICE = 4
+  val NB_FACES = 6
+  val MAX_VALUE = 7
+
+  def slide[A](s: Seq[A]): Iterator[(A, A)] = {
+    Iterator((s.last, s.head)) ++ s.sliding(2).map {
+      case Seq(i, j) => (i, j)
+    }
+  }
+
+  val problem = CSPOM {
+    val dice = for (i <- 0 until NB_DICE) yield {
+      for (j <- 0 until NB_FACES) yield {
+        interVar(s"D${i}F$j", 1, MAX_VALUE)
+      }
+    }
+
+    for (d <- dice) {
+      for (Seq(i, j) <- d.sliding(2)) {
+        ctr(i <= j)
+      }
+    }
+
+    val nbs = freeInt("NBS")
+
+    for ((d1, d2) <- slide(dice)) {
+      ctr(nbs === nbSup(d1, d2))
+
+      for (f1 <- d1; f2 <- d2) {
+        ctr(f1 !== f2)
+      }
+    }
+
+    ctr(nbs > (NB_FACES * NB_FACES) / 2)
+  }
+
+  def nbSup(d1: Seq[IntVariable], d2: Seq[IntVariable]) = {
+    val counts = for (i <- d1; j <- d2) yield {
+      i > j
+    }
+    occurrence(true, counts: _*)
+  }
+
+  val solver = Solver(problem)
+  solver.maximize("NBS")
+
+  for (solution <- solver) {
+    println(solution("NBS"))
+    for (i <- 0 until NB_DICE) {
+      println((0 until NB_FACES).map(j => solution(s"D${i}F$j")))
+    }
+
+  }
+}
